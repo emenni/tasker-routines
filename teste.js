2ee53474afe4459e9e84b5da22b2936754b05aaf
emenni <michelnegrao@gmail.com>
2022-06-23 15:28:11 -0300

    1 file modified

--- teste.js
+++ teste.js
@@ -1,4 +1,15 @@
-function hellow(a) {
-  return [...a] + '!+';
+function readTextFile(file) {
+  var rawFile = new XMLHttpRequest();
+  rawFile.open('GET', file, false);
+  rawFile.onreadystatechange = function () {
+    if (rawFile.readyState === 4) {
+      if (rawFile.status === 200 || rawFile.status == 0) {
+        var allText = rawFile.responseText;
+        alert(allText);
+      }
+    }
+  };
+  rawFile.send(null);
 }
-var result = hellow(['teste']);
+
+readTextFile('file:///Documnents/SMSCartao.json');
