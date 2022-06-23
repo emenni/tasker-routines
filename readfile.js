d01794c4093d5dc381e5647a5ebf4a3b6687f1b3
emenni <michelnegrao@gmail.com>
2022-06-23 16:23:22 -0300

    1 file modified

--- readfile.js
+++ readfile.js
@@ -1,15 +1,1 @@
-function readTextFile(file) {
-  var rawFile = new XMLHttpRequest();
-  rawFile.open('GET', file, false);
-  rawFile.onreadystatechange = function () {
-    if (rawFile.readyState === 4) {
-      if (rawFile.status === 200 || rawFile.status == 0) {
-        var allText = rawFile.responseText;
-        flash(allText);
-      }
-    }
-  };
-  rawFile.send(null);
-}
-
-readTextFile('/Documents/SMSCartao.json');
+var allText = readFile('/Documents/SMSCartao.json');
