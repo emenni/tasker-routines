38627fe62a6f23af3115b0c1fb22008f32a82b97
emenni <michelnegrao@gmail.com>
2022-06-24 18:07:44 -0300

    1 file modified

--- tasker-write-entry.js
+++ tasker-write-entry.js
@@ -2,8 +2,8 @@ function writeNewEntry() {
   var filename = 'Documents/SMSCartao.json';
   var file = readFile(filename);
   var bd = JSON.parse(file);
-  bd.push(http_data);
-  var changes = bd;
+  bd.push(JSON.parse(http_data));
+  var changes = JSON.stringify(bd);
   writeFile(filename, changes, false);
 }
 writeNewEntry();
