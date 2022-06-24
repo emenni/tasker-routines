674fd47cb617fda09f439bc917de3e8f98542e5c
emenni <michelnegrao@gmail.com>
2022-06-24 00:56:50 -0300

    2 files deleted

--- tasker-action-bulk.js
+++
@@ -1,9 +0,0 @@
-function bulkLoad() {
-  var filename = 'Documents/antigos.json';
-  var file = readFile(filename);
-  var bd = JSON.parse(file);
-  for (var i = 0; i < bd.length; i++){
-     SMSRB = bd[0]
-  }
-}
-bulkLoad();
