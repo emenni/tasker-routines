a7fc349e76dc4d515becd8fecd405941b4b9757e
emenni <michelnegrao@gmail.com>
2022-06-25 00:09:11 -0300

    1 file modified

--- tasker-select-send.js
+++ tasker-select-send.js
@@ -2,14 +2,14 @@ function selectToSend() {
   var file = readFile(jsonfile);
   var bd = JSON.parse(file);
 
-var [day, month, year] = startdate.split('/')
-startdate = new Date(+year || new Date().getFullYear(), +month - 1, +day);
+  var [day, month, year] = startdate.split('/');
+  startdate = new Date(+year || new Date().getFullYear(), +month - 1, +day);
 
-[day, month, year] = enddate.split('/')
-enddate = new Date(+year || new Date().getFullYear(), +month - 1, +day);
+  [day, month, year] = enddate.split('/');
+  enddate = new Date(+year || new Date().getFullYear(), +month - 1, +day);
 
-  flash(bd())
-  flash(startdate)
+  flash(bd.toString());
+  //flash(startdate)
 
   var resultProductData = bd.filter(function (a) {
     var hitDates = a.em || {};
