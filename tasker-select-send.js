12d06ef4b6a6cdcc50c939b291ea77615c02ad2c
emenni <michelnegrao@gmail.com>
2022-06-25 00:02:05 -0300

    1 file modified

--- tasker-select-send.js
+++ tasker-select-send.js
@@ -2,9 +2,6 @@ function selectToSend() {
   var file = readFile(jsonfile);
   var bd = JSON.parse(file);
 
-  var startdate = new Date(startdate);
-  var enddate = new Date(enddate);
-
 var [day, month, year] = startdate.split('/')
 startdate = new Date(+year || new Date().getFullYear(), +month - 1, +day);
 
