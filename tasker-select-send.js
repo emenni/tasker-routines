1af16737728171a747613c0f9a68b344888279e2
emenni <michelnegrao@gmail.com>
2022-06-28 15:38:16 -0300

    1 file modified

--- tasker-select-send.js
+++ tasker-select-send.js
@@ -8,7 +8,6 @@ function selectToSend() {
   //day, month, year] = enddate.split('/');
   //enddate = new Date(+year || new Date().getFullYear(), +month - 1, +day);
 
-  file = 'vaziio'
   flash(file);
 
   //flash(bd(0));
