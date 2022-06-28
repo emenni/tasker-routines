1677adfedc09ee16b68e60f34b1f468fba0aece1
emenni <michelnegrao@gmail.com>
2022-06-28 15:28:03 -0300

    1 file modified

--- tasker-select-send.js
+++ tasker-select-send.js
@@ -8,7 +8,7 @@ function selectToSend() {
   //day, month, year] = enddate.split('/');
   //enddate = new Date(+year || new Date().getFullYear(), +month - 1, +day);
 
-  flash(records);
+  flash(bd.values;
 
   //flash(bd(0));
 
