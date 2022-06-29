465966749f42312cd3af7a3eca247be0bfe17c07
emenni <michelnegrao@gmail.com>
2022-06-29 19:16:06 -0300

    1 file modified

--- tasker-bt-found.js
+++ tasker-bt-found.js
@@ -1,48 +1,21 @@
-var leituras = {
-  //bt_alias,
-  //bt_battery_level,
-  //bt_class,
-  //bt_class_name,
-  bt_connected,
-  bt_encrypted,
-  bt_address,
-  bt_major_class,
-  bt_major_class_name,
-  bt_name,
-  bt_paired,
-  bt_signal_strength,
-  bt_type
-};
 gravarLeitura = [];
 
 for (leitura in bt_address) {
-  flash({
-    //btAlias: leituras.bt_alias[leitura] || 'notDeclared',
-    //btBatteryLevel: leituras.bt_battery_level[leitura] || 'notDeclared',
-    //btClass: leituras.bt_class[leitura] || 'notDeclared',
-    //btClassName: leituras.bt_class_name[leitura] || 'notDeclared',
-    btConnected: leituras.bt_connected[leitura] || 'notDeclared',
-    btEncrypted: leituras.bt_encrypted[leitura] || 'notDeclared',
-    btAddress: leituras.bt_address[leitura] || 'notDeclared',
-    btMajorClass: leituras.bt_major_class[leitura] || 'notDeclared',
-    btMajorClassName: leituras.bt_major_class_name[leitura] || 'notDeclared',
-    btName: leituras.bt_name[leitura] || 'notDeclared',
-    btPaired: leituras.bt_paired[leitura] || 'notDeclared',
-    btSignalStrength: leituras.bt_signal_strength[leitura] || 'notDeclared',
-    btType: leituras.bt_type[leitura] || 'notDeclared',
+  gravarLeitura.push({
+    btAlias: bt_alias[leitura],
+    btBatteryLevel : bt_battery_level[leitura] ,
+    btClass: bt_class[leitura] ,
+    btClassName: bt_class_name[leitura] ,
+    btConnected: bt_connected[leitura] ,
+    btEncrypted: bt_encrypted[leitura] ,
+    btAddress: bt_address[leitura],
+    btMajorClass: bt_major_class[leitura] ,
+    btMajorClassName:bt_major_class_name[leitura] ,
+    btName: bt_name[leitura] ,
+    btPaired: bt_paired[leitura] ,
+    btSignalStrength:     bt_signal_strength[leitura] ,
+    btType: bt_type[leitura] ,
   });
 }
 
-/* bt_alias(),
-bt_battery_level(),
-bt_class(),
-bt_class_name(),
-bt_connected(),
-bt_encrypted(),
-bt_address(),
-bt_major_class(),
-bt_major_class_name(),
-bt_name(),
-bt_paired(),
-bt_signal_strength(),
-bt_type(), */
+flash(JSON.stringify(gravarLeitura[0]))
