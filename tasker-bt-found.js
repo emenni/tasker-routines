a25ed11ae1e53262125acf3c48e7baf3a327eeed
emenni <michelnegrao@gmail.com>
2022-06-29 17:40:15 -0300

    1 file modified

--- tasker-bt-found.js
+++ tasker-bt-found.js
@@ -1,19 +1,24 @@
 var leituras = { bt_name, bt_address };
+gravarLeitura = []
 
 for (leitura in bt_address) {
-  flash(leituras.bt_name[leitura] + ' -> ' + leituras.bt_address[leitura]);
+  flash(
+    {
+      "btAlias" : leituras.bt_alias[leitura] || 'notDeclared' ,
+      "btBatteryLevel" : leituras.bt_battery_level[leitura] || 'notDeclared' ,
+      "btClass" : leituras.bt_class[leitura] || 'notDeclared' ,
+      "btClassName" : leituras.bt_class_name[leitura] || 'notDeclared' ,
+      "btConnected" : leituras.bt_connected[leitura] || 'notDeclared' ,
+      "btEncrypted" : leituras.bt_encrypted[leitura] || 'notDeclared' ,
+      "btAddress" : leituras.bt_address[leitura] || 'notDeclared' ,
+      "btMajorClass" : leituras.bt_major_class[leitura] || 'notDeclared' ,
+      "btMajorClassName" : leituras.bt_major_class_name[leitura] || 'notDeclared' ,
+      "btName" : leituras.bt_name[leitura] || 'notDeclared' ,
+      "btPaired" : leituras.bt_paired[leitura] || 'notDeclared' ,
+      "btSignalStrength" : leituras.bt_signal_strength[leitura] || 'notDeclared' ,
+      "btType" : leituras.bt_type[leitura] || 'notDeclared' 
+      }    
+    )
 }
 
-/* bt_alias(),
-bt_battery_level(),
-bt_class(),
-bt_class_name(),
-bt_connected(),
-bt_encrypted(),
-bt_address(),
-bt_major_class(),
-bt_major_class_name(),
-bt_name(),
-bt_paired(),
-bt_signal_strength(),
-bt_type(), */
+
