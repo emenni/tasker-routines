ff4bd9d17e179015f1dc580ea7dc3f3b4c3f4c28
emenni <michelnegrao@gmail.com>
2022-06-29 15:57:37 -0300

    1 file added

---
+++ tasker-bt-found.js
@@ -0,0 +1,17 @@
+bt_address.forEach((valor) => {
+  flash('Teste:' + valor);
+});
+
+/* bt_alias(),
+bt_battery_level(),
+bt_class(),
+bt_class_name(),
+bt_connected(),
+bt_encrypted(),
+bt_address(),
+bt_major_class(),
+bt_major_class_name(),
+bt_name(),
+bt_paired(),
+bt_signal_strength(),
+bt_type(), */
