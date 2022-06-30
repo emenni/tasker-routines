04ae1fbdf42b68405ff05bf4730b793e357361a6
emenni <michelnegrao@gmail.com>
2022-06-29 23:01:09 -0300

    1 file modified

--- tasker-bt-found.js
+++ tasker-bt-found.js
@@ -1,5 +1,4 @@
-(function foundBTDevices(){
-
+(function foundBTDevices() {
   foundDevices = [];
   var timestamp = new Date(Date.now()).toLocaleString();
   for (leitura in bt_address) {
@@ -8,20 +7,23 @@
       location: gl_map_url,
       //btAlias: bt_alias[leitura],
       //btBatteryLevel : bt_battery_level[leitura] ,
-      btClass: bt_class[leitura] ,
-      btClassName: bt_class_name[leitura] ,
-      btConnected: bt_connected[leitura] ,
-      btEncrypted: bt_encrypted[leitura] ,
+      btClass: bt_class[leitura],
+      btClassName: bt_class_name[leitura],
+      btConnected: bt_connected[leitura],
+      btEncrypted: bt_encrypted[leitura],
       btAddress: bt_address[leitura],
-      btMajorClass: bt_major_class[leitura] ,
-      btMajorClassName:bt_major_class_name[leitura] ,
-      btName: bt_name[leitura] ,
-      btPaired: bt_paired[leitura] ,
-      btSignalStrength:     bt_signal_strength[leitura] ,
-      btType: bt_type[leitura] ,
+      btMajorClass: bt_major_class[leitura],
+      btMajorClassName: bt_major_class_name[leitura],
+      btName: bt_name[leitura],
+      btPaired: bt_paired[leitura],
+      btSignalStrength: bt_signal_strength[leitura],
+      btType: bt_type[leitura],
     });
   }
 
+  var file = readFile(jsonfile);
+  var db = JSON.parse(file);
+
   var changes = JSON.stringify(foundDevices);
   writeFile(jsonfile, changes, true);
-})()
+})();
